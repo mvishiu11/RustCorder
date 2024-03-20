6f5d68a438f91710148ff6bec57096b01de5ec27
mvishiu11 <116217532+mvishiu11@users.noreply.github.com>
2024-03-20 21:37:44 +0100

    chore: a little cleanup

--- src/main.rs
+++ src/main.rs
@@ -21,18 +21,15 @@ fn main() -> Result<(), anyhow::Error>
     // };
     println!("Default input config: {:?}", config);
 
-    // Channel for transferring samples from callback to main thread
     let (sender, receiver) = channel();
 
-    // Error handling for the audio stream
     let err_fn = |err| eprintln!("An error occurred on the audio input stream: {}", err);
 
-    // Building the stream
     let stream_config = config.clone().into();
     let stream = device.build_input_stream(
         &stream_config,
         move |data: &[f32], _: &cpal::InputCallbackInfo| {
-            let _ = sender.send(data.to_owned());  // Use '_' to ignore 'send' Result (avoid panic in callback)
+            let _ = sender.send(data.to_owned());
         },
         err_fn,
         None,
@@ -40,10 +37,9 @@ fn main() -> Result<(), anyhow::Error>
     stream.play()?;
 
     let mut recorded_samples: Vec<f32> = Vec::new();
-    let record_time = std::time::Duration::from_secs(20);  // Record for 5 seconds
+    let record_time = std::time::Duration::from_secs(5);
     let start_time = std::time::Instant::now();
 
-    // Recording loop
     while start_time.elapsed() < record_time {
         if let Ok(data) = receiver.try_recv() {
             recorded_samples.extend_from_slice(&data);
@@ -52,10 +48,8 @@ fn main() -> Result<(), anyhow::Error>
 
     println!("Recorded samples count: {}", recorded_samples.len());
 
-    // Stop the stream
     stream.pause()?;
 
-    // Saving the recorded data
     let spec = hound::WavSpec {
         channels: config.channels(),
         sample_rate: config.sample_rate().0,
@@ -66,9 +60,9 @@ fn main() -> Result<(), anyhow::Error>
     let writer = BufWriter::new(File::create("output.wav")?);
     let mut wav_writer = hound::WavWriter::new(writer, spec)?;
     for sample in recorded_samples {
-        wav_writer.write_sample(sample)?;  // Convert f32 audio sample to i16 and write it
+        wav_writer.write_sample(sample)?;
     }
-    wav_writer.finalize()?;  // Ensure header and data are flushed and file is closed
+    wav_writer.finalize()?;
 
     println!("Recording is saved to 'output.wav'");
     Ok(())
